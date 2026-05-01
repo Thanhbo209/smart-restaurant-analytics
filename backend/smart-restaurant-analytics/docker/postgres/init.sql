fcc271bb10d0257ce614018802fdbbf42b33bbe6
Thanh Pham <thanhagar123@gmail.com>
2026-05-01 22:15:59 +0700

    chore: setup dockerfile and flyway db control

---
+++ backend/smart-restaurant-analytics/docker/postgres/init.sql
@@ -0,0 +1,3 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Generate UUID and used for ID in db
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements"; -- Tracking query performance, analytics
+
---
+++ backend/smart-restaurant-analytics/src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE categories (
+    id          BIGSERIAL PRIMARY KEY,
+    name        VARCHAR(150)  NOT NULL,
+    slug        VARCHAR(150)  NOT NULL UNIQUE,
+    description VARCHAR(500),
+    is_active   BOOLEAN       NOT NULL DEFAULT TRUE,
+    parent_id   BIGINT        REFERENCES categories(id),
+    created_at  TIMESTAMP     NOT NULL,
+    updated_at  TIMESTAMP     NOT NULL
+);
+
+CREATE TABLE products (
+    id              BIGSERIAL PRIMARY KEY,
+    name            VARCHAR(200)   NOT NULL,
+    slug            VARCHAR(200)   NOT NULL,
+    price           NUMERIC        NOT NULL,
+    cost            NUMERIC,
+    description     TEXT,
+    sku             VARCHAR(100)   UNIQUE,
+    image_url       VARCHAR(500),
+    image_public_id VARCHAR(200),
+    is_active       BOOLEAN        NOT NULL DEFAULT TRUE,
+    is_available    BOOLEAN        NOT NULL DEFAULT TRUE,
+    category_id     BIGINT         NOT NULL REFERENCES categories(id),
+    created_at      TIMESTAMP      NOT NULL,
+    updated_at      TIMESTAMP      NOT NULL
+);
