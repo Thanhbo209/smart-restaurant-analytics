561809a41cdfa170ca0cd9bfaebb4d2509f9b66f
Thanh Pham <thanhagar123@gmail.com>
2026-05-01 13:34:56 +0700

    first commit

---
+++ backend/smart-restaurant-analytics/src/main/java/com/thanhpham/smart_restaurant_analytics/SmartRestaurantAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.thanhpham.smart_restaurant_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRestaurantAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRestaurantAnalyticsApplication.class, args);
+	}
+
+}
---
+++ backend/smart-restaurant-analytics/src/test/java/com/thanhpham/smart_restaurant_analytics/SmartRestaurantAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhpham.smart_restaurant_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRestaurantAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
